f43f608767f08897cf41c44da45ef5e35aefc5c9
Li Yu <li.yu.unsw@gmail.com>
2020-05-02 15:14:57 +1000

    add yml - build

--- src/test/java/com/yulikarate/test/testruns/DemoRunner.java
+++ src/test/java/com/yulikarate/test/testruns/DemoRunner.java
@@ -6,8 +6,17 @@ import cucumber.api.CucumberOptions;
 import org.junit.runner.RunWith;
 
 
+//@RunWith(Karate.class)
+////@CucumberOptions(features = "classpath:com.yulikarate.test.testruns/demo/demo.feature" ,format={"pretty","html:reports","json:report.json"})
+//@CucumberOptions(features = "src/test/resources/demo.feature" ,format={"pretty","html:reports","json:report.json"})
+//
+//public class DemoRunner extends KarateRunner {
+//
+//}
+
+
 @RunWith(Karate.class)
-@CucumberOptions(features = "classpath:com.yulikarate.test.testruns/demo/demo.feature" ,format={"pretty","html:reports","json:report.json"})
+@CucumberOptions(features = "classpath:resources/weather.feature",format={"pretty","html:reports","json:report.json"})
 
 public class DemoRunner extends KarateRunner {
 
